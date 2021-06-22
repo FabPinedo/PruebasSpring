9e90e4d4c13736a78604f4a5b2568c89c70e7fde
FabPinedo <renzopinedoespin@gmail.com>
2021-06-22 15:11:47 -0500

    Actualizacion22/06/2021

--- ConexionBD2/src/main/java/com/example/demo/models/dao/IpaisDao.java
+++ ConexionBD2/src/main/java/com/example/demo/models/dao/IpaisDao.java
@@ -1,10 +1,11 @@
 package com.example.demo.models.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.models.entity.Pais;
-
-public interface IpaisDao extends CrudRepository<Pais,Integer>{
+@Repository
+public interface IpaisDao extends CrudRepository<Pais,Long>{
 //Recordar repasar metodos de CRUDREPOSITORY
 	//Aqui escribir cualquier logica del negocio q se requiera(query)
 }
--- ConexionBD2/src/main/java/com/example/demo/models/service/paisServicempl.java
+++ ConexionBD2/src/main/java/com/example/demo/models/service/paisServicempl.java
@@ -2,18 +2,22 @@ package com.example.demo.models.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.models.dao.IpaisDao;
 import com.example.demo.models.entity.Pais;
 
 @Service
 public class paisServicempl implements IpaisService{
-	private IpaisService PaisDao;
+	@Autowired
+	private IpaisDao PaisDao;
 
 	@Override
 	@Transactional(readOnly = true)
 	public List<Pais> findAll() {
-		return (List<Pais>) PaisDao.findAll();
+		return (List<Pais>) PaisDao.findAll();//Posible error para el postman
 	}
 	
 }
